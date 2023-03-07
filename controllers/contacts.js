ac45cbf02c337cfe02db28efbdfeb4f995f327df
Artem Karpenko <dante120591@gmail.com>
2023-03-07 22:47:52 +0200

    Now done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,20 +1,26 @@
 const { Contact } = require("../models/contact");
 
 const getAll = async (req, res) => {
-  const data = await Contact.find();
-  res.send(data);
+  const { _id } = req.user;
+  const data = await Contact.find({ owner: _id });
+  res.json(data);
 };
 
 const addContact = async (req, res) => {
   const { name, number } = req.body;
-  const newContact = await Contact.create({ name, number });
-  res.status(201).send(newContact);
+  const { _id } = req.user;
+  const newContact = await Contact.create({
+    name,
+    number,
+    owner: _id,
+  });
+  res.status(201).json(newContact);
 };
 
 const deleteContact = async (req, res) => {
   const { id } = req.params;
   const deletedContact = await Contact.findByIdAndDelete({ _id: id });
-  res.send(deletedContact);
+  res.json(deletedContact);
 };
 
 module.exports = { getAll, addContact, deleteContact };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,9 +4,10 @@ const {
   addContact,
   deleteContact,
 } = require("../../controllers/contacts");
+const { auth } = require("../../middleware/auth");
 
-router.get("/", getAll);
-router.post("/add", addContact);
-router.delete("/:id", deleteContact);
+router.get("/", auth, getAll);
+router.post("/add", auth, addContact);
+router.delete("/:id", auth, deleteContact);
 
 module.exports = router;
